40b13f38c2d38b0fe63d2f99c766ea33da624435
maunikrghvndr <maunik.rghvndr@gmail.com>
2020-11-02 01:32:40 -0800

    - routing fixed

--- Angular/resume-angular-app/ClientApp/src/app/app.component.ts
+++ Angular/resume-angular-app/ClientApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { CounterComponent } from 'src/app/counter/counter.component';
+import { HomeComponent } from 'src/app/home/home.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
